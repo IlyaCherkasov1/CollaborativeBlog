4996351f0215a79c9dd4f3b155005efa6263530f
Илья Черкасов <Ilya.Chercasov@gmail.com>
2021-11-10 10:08:25 +0300

    create model

---
+++ Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollaborativeBlog.Models
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public int TagName { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
